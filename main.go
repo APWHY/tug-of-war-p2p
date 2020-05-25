d6d36a3dc7b63c9cff11080376e7cf6fcbc12e12
Thomas Kilkelly <thomas.kilkelly@bcgdv.com>
2020-05-25 18:54:07 +1000

    cleaned up main.go

--- main.go
+++ main.go
@@ -20,13 +20,9 @@ func main() {
 	router.Static("/assets", "./assets")
 
 	router.GET("/", func(c *gin.Context) {
-		// Call the HTML method of the Context to render a template
 		c.HTML(
-			// Set the HTTP status to 200 (OK)
 			http.StatusOK,
-			// Use the index.html template
 			"index.html",
-			// Pass the data that the page uses (in this case, 'title')
 			gin.H{
 				"title": "Home Page",
 				"err":   "",
@@ -35,14 +31,12 @@ func main() {
 
 	})
 	router.GET("/lobby", func(c *gin.Context) {
-		// Call the HTML method of the Context to render a template
 		question := c.Query("question")
 		firstOption := c.Query("first-opt")
 		secondOption := c.Query("second-opt")
 
 		// redirect back to the home page if all required parameters are not provided
 		if question == "" || firstOption == "" || secondOption == "" {
-			log.Println("showing home page???????????????????????????????????????")
 			c.HTML(
 				http.StatusOK,
 				"index.html",
@@ -56,11 +50,6 @@ func main() {
 
 			newLobby := lobby.CreateLobby(question, firstOption, secondOption)
 			lobbies[newLobby.ID.String()] = newLobby
-			log.Println(*newLobby, " is lobby")
-			// // jsonLobby, err := json.Marshal(newLobby)
-			// if err != nil {
-			// 	panic("can't marshal the lobby!")
-			// }
 			c.HTML(
 				http.StatusOK,
 				"lobby.html",
@@ -80,9 +69,7 @@ func main() {
 		ID := c.Query("lobbyId")
 
 		if foundLobby, ok := lobbies[ID]; ok {
-			log.Println("found lobby with id")
 			newParticipant := foundLobby.AddParticipant()
-			log.Println("name:", newParticipant)
 			c.HTML(
 				http.StatusOK,
 				"clicker.html",
@@ -97,7 +84,6 @@ func main() {
 				},
 			)
 		} else {
-			log.Println("invalid lobby id received: ", ID)
 			c.HTML(
 				http.StatusOK,
 				"index.html",
@@ -114,16 +100,10 @@ func main() {
 		lobbyID := c.Query("lobbyId")
 		participantID := c.Query("participantId")
 		if foundLobby, ok := lobbies[lobbyID]; ok {
-			log.Println("found lobby: ", lobbyID)
-
 			if participantID == "" {
-				log.Println("returning lobby ws: ", lobbyID)
 				foundLobby.MakeLobbyWsHandler(c.Writer, c.Request)
 			} else {
-				if foundLobby.MakeParticipantWsHandler(c.Writer, c.Request, participantID) {
-					log.Println("found participant: ", lobbyID)
-				} else {
-					log.Println("invalid participant id received: ", participantID)
+				if !foundLobby.MakeParticipantWsHandler(c.Writer, c.Request, participantID) {
 					c.HTML(
 						http.StatusOK,
 						"index.html",
@@ -135,7 +115,6 @@ func main() {
 				}
 			}
 		} else {
-			log.Println("invalid lobby id received: ", lobbyID)
 			c.HTML(
 				http.StatusOK,
 				"index.html",
